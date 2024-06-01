805786c65a9f7c43ad69c5d3d8f0c6b59c316d75
wequal <wequalw@supermicro.com>
2024-06-01 15:20:38 -0700

    add motor control.\n re-layout the web page

--- app.py
+++ app.py
@@ -7,15 +7,27 @@ app.secret_key = 'your_secret_key'  # Replace with your actual secret key
 # Setup GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+
+# Define GPIO pins
 led_pin = 18
+up_pin = 17
+down_pin = 27
+left_pin = 22
+right_pin = 23
+
+# Set GPIO pins as output
 GPIO.setup(led_pin, GPIO.OUT)
+GPIO.setup(up_pin, GPIO.OUT)
+GPIO.setup(down_pin, GPIO.OUT)
+GPIO.setup(left_pin, GPIO.OUT)
+GPIO.setup(right_pin, GPIO.OUT)
 
 # MJPEG Streamer URL
 STREAM_URL = 'http://192.168.2.169:8080/?action=stream'
 
 # Dummy user credentials
-USERNAME = 'admin'
-PASSWORD = 'password'
+USERNAME = 'ADMIN'
+PASSWORD = 'ADMIN'
 
 @app.route('/')
 def index():
@@ -50,6 +62,33 @@ def toggle_led():
         GPIO.output(led_pin, GPIO.HIGH)
     else:
         GPIO.output(led_pin, GPIO.LOW)
+    return redirect(url_for('index'))
+
+@app.route('/control_motor', methods=['POST'])
+def control_motor():
+    if 'logged_in' not in session:
+        return redirect(url_for('login'))
+    
+    direction = request.form['direction']
+    if direction == 'up':
+        GPIO.output(up_pin, GPIO.HIGH)
+    elif direction == 'down':
+        GPIO.output(down_pin, GPIO.HIGH)
+    elif direction == 'left':
+        GPIO.output(left_pin, GPIO.HIGH)
+    elif direction == 'right':
+        GPIO.output(right_pin, GPIO.HIGH)
+    return redirect(url_for('index'))
+
+@app.route('/stop_motor', methods=['POST'])
+def stop_motor():
+    if 'logged_in' not in session:
+        return redirect(url_for('login'))
+    
+    GPIO.output(up_pin, GPIO.LOW)
+    GPIO.output(down_pin, GPIO.LOW)
+    GPIO.output(left_pin, GPIO.LOW)
+    GPIO.output(right_pin, GPIO.LOW)
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
